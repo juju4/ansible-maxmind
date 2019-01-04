67a74987f0270cacffa16333df4625e4ccf0cf4d
Your Name <you@example.com>
2019-01-04 10:20:48 +0000

    remove some db path 2, test update - maxmind geolite discontinuation

--- test/integration/default/serverspec/maxminddb_spec.rb
+++ test/integration/default/serverspec/maxminddb_spec.rb
@@ -3,9 +3,6 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe file('/var/maxmind/GeoIP.dat') do
-  it { should be_readable }
-end
 describe file('/var/maxmind/GeoIPASNum.dat') do
   it { should be_readable }
 end
@@ -19,5 +16,11 @@ describe file('/var/maxmind/GeoLiteCity.dat') do
   it { should be_readable }
 end
 describe file('/var/maxmind/GeoLiteCityv6.dat') do
+  it { should be_readable }
+end
+describe file('/var/maxmind/GeoLite2-City.mmdb') do
+  it { should be_readable }
+end
+describe file('/var/maxmind/GeoLite2-Country.mmdb') do
   it { should be_readable }
 end
