2f2625d6c9161fe3815a3b40da81f164821ec3ab
juju4 <juju4@users.noreply.github.com>
2019-01-20 18:58:00 +0000

    remove last remnants - maxmind geolite legacy discontinuation

--- test/integration/default/serverspec/maxminddb_spec.rb
+++ test/integration/default/serverspec/maxminddb_spec.rb
@@ -3,21 +3,6 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe file('/var/maxmind/GeoIPASNum.dat') do
-  it { should be_readable }
-end
-describe file('/var/maxmind/GeoIPASNumv6.dat') do
-  it { should be_readable }
-end
-describe file('/var/maxmind/GeoIPv6.dat') do
-  it { should be_readable }
-end
-describe file('/var/maxmind/GeoLiteCity.dat') do
-  it { should be_readable }
-end
-describe file('/var/maxmind/GeoLiteCityv6.dat') do
-  it { should be_readable }
-end
 describe file('/var/maxmind/GeoLite2-City.mmdb') do
   it { should be_readable }
 end
